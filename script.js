f443d0f2e4637899d45477a366469296c9ba6e8d
Vipin Sharma <vipin2112work@gmail.com>
2024-04-01 17:28:26 +0530

    add grid button

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 const container = document.querySelector('#container');
 
+// createGrid function
 
-
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        let box = document.createElement("div");
-        box.classList.add("box");
-        container.appendChild(box);
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let box = document.createElement("div");
+            box.classList.add("box");
+            container.appendChild(box);
+            box.style.flex = `0 0 calc(100% / ${size})`;
+        }
     }
+
+
+    // Hover 
+
+    const boxes = document.querySelectorAll('.box');
+
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', () => {
+            box.classList.add("hovered");
+        });
+    });
+
 }
+
+
+// Grid button
+
+const grid = document.querySelector('.grid');
+grid.addEventListener('click', function () {
+    let gridSize = prompt("Enter the size for the grid (16-100)");
+
+    // delete current grid
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        container.removeChild(box);
+    });
+
+    createGrid(gridSize);
+
+});
+
+
+// initial grid
+
+createGrid(16);
